9130cf47ed898651e8c47602d0ad3910e3df3333
Erick <erick.nahuelpaiva.001@gmail.com>
2024-12-13 20:31:31 -0300

    agregada modificacion para permitir origenes especificos

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
-// server.js
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 import { checkDatabaseConnection } from './utils/dbUtils.js';
 
 const app = express();
 
-// Middleware
+// Configuración de CORS
+const allowedOrigin = 'http://localhost:5173'; // Cambia esto a la URL de tu app web
+app.use(cors({
+  origin: (origin, callback) => {
+    // Bloquea orígenes vacíos (Postman, Thunder Client) y permite solo el permitido
+    /* if (!origin) {
+      return callback(new Error('No origin allowed'), false);
+    }
+    if (origin === allowedOrigin) {
+      return callback(null, true);
+    } */
+    // para permitir thunder client
+    if (!origin || origin === allowedOrigin) {
+        return callback(null, true);
+      }
+    callback(new Error('Origin not allowed'), false);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  credentials: true, // Si necesitas enviar cookies o encabezados de autenticación
+}));
+
+// Middleware para parsear JSON
 app.use(express.json());
 
-// Routes
-app.use("/user",userRoutes)
+// Rutas
+app.use("/user", userRoutes);
 
-app.get("", (req, res)=>{
-    res.send('hola');
-})
+app.get("", (req, res) => {
+  res.send('hola');
+});
 
+// Verificar conexión a la base de datos
 checkDatabaseConnection();
 
 export default app;
