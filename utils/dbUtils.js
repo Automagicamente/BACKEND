d1146d792653bd07a4ac5b6e00566726ca39937e
Erick <erick.nahuelpaiva.001@gmail.com>
2024-12-19 19:45:15 -0300

    query para crear tabla inicialmente modificado

--- utils/dbUtils.js
+++ utils/dbUtils.js
@@ -4,23 +4,28 @@ export const checkDatabaseConnection = async () => {
   try {
     const connection = await pool.getConnection();
     console.log('Database connected successfully!');
-    const createTable = await pool.query(`
-        CREATE TABLE IF NOT EXISTS users (
-          id INT AUTO_INCREMENT PRIMARY KEY,
-          name VARCHAR(100) NOT NULL,
-          email VARCHAR(100) NOT NULL UNIQUE,
-          password VARCHAR(255) NOT NULL,
-          comment TEXT,
-          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-          updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        );
-      `);
-    console.log("resultado de crear tabla",createTable);
-    
     connection.release(); // Liberar la conexión después de verificarla
   } catch (error) {
     console.error('Database connection failed:', error.message);
     console.log('Retrying connection in 3 seg...');
     setTimeout(checkDatabaseConnection, 3000); // 3 minutos en milisegundos
+  }
+
+  try {
+    const createTable = await pool.query(`
+      CREATE TABLE users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        comment TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+      );
+    `);
+    console.log("resultado de crear tabla", createTable);
+  } catch (error) {
+    console.log("tabla existe");
+    
   }
 };
